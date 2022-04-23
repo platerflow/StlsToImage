2e494884b534b046f500f95011447aab99af56c0
Gijs van Dulmen <gijs@vandulmen.net>
2022-04-23 15:09:29 +0200

    Add first version

---
+++ index.js
@@ -0,0 +1,26 @@
+import DirectoryFile from "./lib/directoryfile.js";
+import Thumbnailer from "./lib/thumbnailer.js";
+
+const StlsToImage = class StlsToImage {
+    constructor() {
+
+    }
+
+    run() {
+        if ( process.argv[2] == undefined ) {
+            console.log("No directory given as parameter");
+            process.exit();
+        } else {
+            const files = new DirectoryFile();
+            const thumbnailer = new Thumbnailer();
+            files.recursiveRead(process.argv[2], item => {
+                if ( item.file.endsWith(".stl") ) {
+                    thumbnailer.addThumbnail(item.path, item.path+".png", 200, 200, 0xba4b36);
+                }
+            });
+        }
+    }
+}
+
+const app = new StlsToImage();
+app.run();
